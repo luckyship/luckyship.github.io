e41fd2f591aec2a25c6af6ef99be4f34011df40b
Xu Chao <xuchao1@flkj.ai>
2023-06-15 09:38:26 +0800

    fix: 自定义hexo扩展文件

---
+++ scripts/hexo-update-time.js
@@ -0,0 +1,42 @@
+const path = require("path");
+const execSync = require("child_process").execSync;
+const moment = require("moment-timezone");
+
+hexo.extend.filter.register("before_post_render", (data) => {
+  // const originDate = data.date;
+  // const gitDate = getDate(data);
+  // if (gitDate < originDate) {
+  //   data.date = gitDate;
+  // }
+
+  const originUpdated = data.updated;
+  const gitUpdated = getUpdated(data);
+  // if (gitUpdated < originUpdated) {
+  data.updated = gitUpdated;
+  // }
+
+  return data;
+});
+
+function getDate(data) {
+  const filePath = getFilePath(data);
+  const date = execSync(`git log --follow --format="%ad" -- ${filePath} | tail -1`).toString().trim();
+  // If the file is created a moment ago, it will be an untracked file, then git can not log it
+  if (date === "") {
+    return moment();
+  }
+  return moment(new Date(date));
+}
+
+function getUpdated(data) {
+  const filePath = getFilePath(data);
+  const updated = execSync(`git log --follow -1 --format="%ad" -- ${filePath}`).toString().trim();
+  if (updated === "") {
+    return moment();
+  }
+  return moment(new Date(updated));
+}
+
+function getFilePath(data) {
+  return path.resolve(hexo.config.source_dir, data.source);
+}
